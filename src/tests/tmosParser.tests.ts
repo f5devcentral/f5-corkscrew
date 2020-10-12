722c2adc96c61c0c24834536894522dcca6cf50e
DumpySquare <bunookana@gmail.com>
2020-10-12 06:46:15 -0500

    events figured out

--- src/tests/tmosParser.tests.ts
+++ src/tests/tmosParser.tests.ts
@@ -2,46 +2,95 @@
 'use strict';
 
 import assert from 'assert';
-import * as fs from 'fs';
 import * as path from 'path';
+// import fetch from 'fetch'
 
-import { parseTmosConfig } from '../tmosParser';
+import { Parser } from '../tmosParse2';
+import { unPacker } from '../unPacker';
 
-
-// const iRuleWithPools = fs.readFileSync(path.join(__dirname, "./artifacts/pools.irule"), "utf-8");
-// const iRuleNoRef = fs.readFileSync(path.join(__dirname, "./artifacts/pools_noRef.irule"), "utf-8");
-
-const devCloud01 = fs.readFileSync(path.join(__dirname, "./artifacts/devCloud01_10.7.2020.conf"), "utf-8");
-const monster = fs.readFileSync(path.join(__dirname, "../../private/monster_bigip.conf"), "utf-8");
-
-
-describe('tmos config parser tests', function() {
-
-    it(`tmos config file to json tree`, function() {
-        const resp = parseTmosConfig(devCloud01);
-
-        const expected = {
-            totalObjectCount: 154,
-            ltmObjectCount: 0,
-            lineCount: 1125,
-        }
-
-        assert.deepStrictEqual(resp.totalObjectCount, expected.totalObjectCount);
-        assert.deepStrictEqual(resp.lineCount, expected.lineCount);
+let unPacked;
+describe('New parse function tests', async () => {
+    
+    /**
+     * These tests were just to provide a platform for adding events to the parser function
+     * 
+     * The function was isolated in the cooresponding file, refactored to work, then refactored for events.
+     * 
+     * This just demonstrates that workflow
+     * 
+     * In short, events need to be emitted at the top level class so they can be listened to by whatever is calling the funtion.
+     * 
+     * This updated function will get factored back into the main ltm class so events can be emitted for other actions, like unpack and app extraction
+     */
+    
+    it(`unpack config from mini ucs`, async () => {
+        
+        unPacked = await unPacker(path.join(__dirname, "./mini_ucs.tar.gz"));
+        const converted = [ unPacked[0].fileName, unPacked[2].size, unPacked[4].fileName ];
+        const expected = ['config/bigip.conf', 341, 'config/partitions/foo/bigip.conf' ];
+        assert.deepStrictEqual(converted, expected)
     });
 
+    it(`parse config files/objects to json tree`, async () => {
+        
+        
+        const newPar = new Parser()
+
+        const parsedEvents = []
+        newPar.on('parseStart', x => parsedEvents.push(x) )
+        newPar.on('parseFile', x => parsedEvents.push(x) )
+        newPar.on('parseEnd', x => parsedEvents.push(x) )
+        
+        const result = newPar.loadParse(unPacked);
+        assert.deepStrictEqual('x', 'x');
+    });
+    
     it(`tmos config file to json tree - monster config`, function() {
-        const resp = parseTmosConfig(monster);
-
-        const expected = {
-            totalObjectCount: 0,
-            ltmObjectCount: 12804,
-            lineCount: 222681,
-        }
-
-        assert.deepStrictEqual(resp.ltmObjectCount, expected.ltmObjectCount);
-        assert.deepStrictEqual(resp.lineCount, expected.lineCount);
+        
+        assert.deepStrictEqual('x', 'x');
     });
-
-
 });
+
+
+
+
+
+
+
+
+
+
+// /**
+//  * the following were tests for the first parsing function outside the main ltm class
+//  * Took about 8 hours to explore this approach which resulted in worse performance than
+//  *  the original method - kept running out of memory and erroring out - had to narrow scope
+//  *  to just ltm configuration objects.  - really just keeping this around for history... for now
+//  */
+// import { parseTmosConfig } from '../tmosParser';
+// const devCloud01 = fs.readFileSync(path.join(__dirname, "./artifacts/devCloud01_10.7.2020.conf"), "utf-8");
+// const monster = fs.readFileSync(path.join(__dirname, "../../private/monster_bigip.conf"), "utf-8");
+// describe('parseTmosConfig - tmos config parser tests', function() {
+//     it(`tmos config file to json tree`, function() {
+//         const resp = parseTmosConfig(devCloud01);
+
+//         const expected = {
+//             totalObjectCount: 154,
+//             ltmObjectCount: 0,
+//             lineCount: 1125,
+//         }
+
+//         assert.deepStrictEqual(resp.totalObjectCount, expected.totalObjectCount);
+//         assert.deepStrictEqual(resp.lineCount, expected.lineCount);
+//     });
+
+//     it(`tmos config file to json tree - monster config`, function() {
+//         const resp = parseTmosConfig(monster);
+//         const expected = {
+//             totalObjectCount: 0,
+//             ltmObjectCount: 12804,
+//             lineCount: 222681,
+//         }
+//         assert.deepStrictEqual(resp.ltmObjectCount, expected.ltmObjectCount);
+//         assert.deepStrictEqual(resp.lineCount, expected.lineCount);
+//     });
+// });
--- src/tmosParse2.ts
+++ src/tmosParse2.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+'use strict';
+
+import { EventEmitter } from 'events';
 import logger from './logger';
 import { countObjects } from './objCounter';
 import { RegExTree } from './regex';
@@ -5,86 +9,127 @@ import { ConfigFiles } from './unPacker'
 import { nestedObjValue, deepMergeObj } from './utils/objects';
 
 
-/**
- * new parsing function to extract from main ltm class and add events for progress
- */
-export function parse(files: ConfigFiles) {
-    const startTime = process.hrtime.bigint();
-    logger.debug('Begining to parse configs')
+export class Parser extends EventEmitter {
+    objectCount: number;
+    tmosVersion: string;
+    rx: any;
+    configObject: unknown;
+    parseTime: number;
 
-    files.forEach(el => {
-        /**
-         * for each file
-         * 1. get tmos version
-         * 2. extract parent objects to array
-         * 3. convert array to main obj
-         */
-
-        if (this.rx) {
-            // rex tree already assigned, lets confirm subsequent file tmos version match
-            if (this.tmosVersion === this.getTMOSversion(el.content, this.rx.tmosVersion)) {
-                // do nothing, current file version matches existing files tmos verion
-            } else {
-                logger.error(`Parsing [${el.fileName}], tmos version of this file does not match previous file [${this.tmosVersion}]`)
-                return
-            }
-        } else {
-            
-            // first time through - build everything
-            const rex = new RegExTree();  // instantiate regex tree
-            this.tmosVersion = getTMOSversion(el.content, rex.tmosVersionReg);  // get tmos version
-            logger.info(`Recieved .conf file of version: ${this.tmosVersion}`)
-            
-            // assign regex tree for particular version
-            this.rx = rex.get(this.tmosVersion)
-        }
-
-        let configAsSingleLevelArray = [];
-        try {
-            // try to parse the config into an array
-            configAsSingleLevelArray = [...el.content.match(this.rx.parentObjects)];
-        } catch (e) {
-            logger.error('failed to extract any parent matches from file - might be a scripts/iapps file...');
-        }
-
-        // if we parsed a config file that produced parent objects
-        if (configAsSingleLevelArray) {
-
-            // get number of config objects
-            const lines = configAsSingleLevelArray.length;
-            logger.debug(`detected ${this.stats.objectCount} parent objects in this file`)
-            
-            // add to main stats
-            this.stats.objectCount += lines;
-            
-            logger.debug(`creating more detailed arrays/objects for deeper inspection`)
-            configAsSingleLevelArray.forEach(el => {
-
-                // extract object name from body
-                const name = el.match(this.rx.parentNameValue);
-
-                if (name && name.length === 3) {
+    constructor() {
+        super();
+    }
     
-                    // split extracted name element by spaces
-                    const names = name[1].split(' ');
-                    // create new nested objects with each of the names, assigning value on inner-most
-                    const newObj = nestedObjValue(names, name[2]);
+    loadParse (files: ConfigFiles) {
+        return this.parse(files);
+    }
 
-                    this.configMultiLevelObjects = deepMergeObj(this.configMultiLevelObjects, newObj);
+    /**
+     * new parsing function to extract from main ltm class and add events for progress
+     * 
+     * This is the latest work as of 10.11.2020
+     */
+    private parse(files: ConfigFiles) {
+        const startTime = process.hrtime.bigint();
+        logger.debug('Begining to parse configs')
+        let objectCount = 0;
+
+        this.emit('parseStart', 'starting to parse stuff')
+
+        files.forEach((el, index) => {
+
+            //  for each file
+            //  1. get tmos version
+            //  2. extract parent objects to array
+            //  3. convert array to main obj
+
+            // create parsing details obj for emitter
+            const parsing = {
+                parsing: el.fileName,   // current file name
+                num: index + 1,         // file #
+                of: files.length        // total # of files
+            }
+            
+            this.emit('parseFile', parsing )
+
+            if (this.rx) {
+                // rex tree already assigned, lets confirm subsequent file tmos version match
+                if (this.tmosVersion === getTMOSversion(el.content, this.rx.tmosVersion)) {
+                    // do nothing, current file version matches existing files tmos verion
+                } else {
+                    logger.error(`Parsing [${el.fileName}], tmos version of this file does not match previous file [${this.tmosVersion}]`)
+                    return
                 }
-            });
+            } else {
+                
+                // first time through - get initial rex tree
+                const rex = new RegExTree();  // instantiate regex tree
+                this.tmosVersion = getTMOSversion(el.content, rex.tmosVersionReg);  // get tmos version
+                logger.info(`Recieved .conf file of version: ${this.tmosVersion}`)
+                
+                // assign regex tree for particular version
+                this.rx = rex.get(this.tmosVersion)
+            }
 
-        }
-    });
+            let configArray = [];
+            try {
+                // try to parse the config into an array
+                configArray = [...el.content.match(this.rx.parentObjects)];
+            } catch (e) {
+                logger.error('failed to extract any parent matches from file - might be a scripts/iapps file...');
+            }
 
-    // get ltm object counts
-    this.stats.objects = countObjects(this.configMultiLevelObjects);
+            // if we parsed a config file that produced parent objects
+            if (configArray) {
 
-    // end processing time, convert microseconds to miliseconds
-    this.stats.parseTime = Number(process.hrtime.bigint() - startTime) / 1000000; 
+                // get number of config objects
+                const lines = configArray.length;
+                logger.debug(`detected ${lines} parent objects in this file`)
+                
+                // add to main stats
+                objectCount += lines;
+                
+                logger.debug(`creating more detailed arrays/objects for deeper inspection`)
+                configArray.forEach(el => {
 
-    return this.stats.parseTime;
+                    // emit events about each object being parsed
+
+                    // extract object name from body
+                    const name = el.match(this.rx.parentNameValue);
+
+                    if (name && name.length === 3) {
+        
+                        // split extracted name element by spaces
+                        const names = name[1].split(' ');
+                        // create new nested objects with each of the names, assigning value on inner-most
+                        const newObj = nestedObjValue(names, name[2]);
+
+                        this.configObject = deepMergeObj(this.configObject, newObj);
+                        
+                    }
+                });
+
+            }
+        });
+
+        // get ltm object counts
+        const objects = countObjects(this.configObject);
+
+        // end processing time, convert microseconds to miliseconds
+        const parseTime = Number(process.hrtime.bigint() - startTime) / 1000000; 
+
+        this.emit('parseEnd', `we done finished, took ${this.parseTime}ms`)
+
+        return {
+            parseTime,
+            objectCount,
+            objects,
+            configObject: this.configObject
+        };
+    }
 }
+
+
 
 /**
  * extract tmos config version from first line
